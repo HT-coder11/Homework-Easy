44ece5af8dfef3ec7082ff94a3bfdb666b0c1153
HT-coder11 <118806265+HT-coder11@users.noreply.github.com>
2024-05-28 18:01:20 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//Create js for recognising audio and convert it to text
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+
+function start(){
+    document.getElementById("fixed").innerHTML = "Do you want to start using AI :) Well if you didn't, too bad, it's starting."
+    recognition.start()
+}
+
+recognition.onresult = function(event){
+    console.log(event)
+    var content = event.results[0][0].transcript
+    document.getElementById("fixeded").innerHTML = "You want so say... "+"<br>"+ content
+
+}
