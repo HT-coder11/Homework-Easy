d26596fc9985605dc74d9eecb5b17238e67c1a53
HT-coder11 <118806265+HT-coder11@users.noreply.github.com>
2024-05-28 18:12:23 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Create js for recognising audio and convert it to text
 var SpeechRecognition = window.webkitSpeechRecognition;
 var recognition = new SpeechRecognition();
-
+var final = []
 
 function start(){
     document.getElementById("fixed").innerHTML = "Do you want to start using AI :) Well if you didn't, too bad, it's starting."
@@ -11,6 +11,7 @@ function start(){
 recognition.onresult = function(event){
     console.log(event)
     var content = event.results[0][0].transcript
-    document.getElementById("fixeded").innerHTML = "You want so say... "+"<br>"+ content
+    final.push(content)
+    document.getElementById("fixeded").innerHTML = "You want so say... "+"<br>"+ final
 
 }
